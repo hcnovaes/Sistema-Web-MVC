9d59d8e24a06afa899fb283dd36f44bea9e450e6
Henrique Novaes <novaeshenrique@outlook.com>
2020-10-02 10:51:39 -0300

    implementando a busca simples

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -2,11 +2,7 @@
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 using SalesWebMvc.Services.Exceptions;
-using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
